450a8419b7e53f98743e322c23e68ede8aec5605
ChaseTeague <teaguechase@yahoo.com>
2023-08-26 21:00:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(value, driver){
+    return value.filter(
+        (maybeMatch) => maybeMatch.toLowerCase() === driver.toLowerCase()
+        );
+}
+
+function fuzzyMatch(value, string){
+    return value.filter(
+        (maybeMatch) => maybeMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+        );
+}
+
+function matchName(value, name){
+    return value.filter(
+        (match) => match.name === name
+    );
+} 
